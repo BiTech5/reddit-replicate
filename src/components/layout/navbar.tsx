721efbd716cbc0e23ec884a8cf89f300dbd43e93
Bikram <bikikc7@gmail.com>
2025-05-15 15:10:31 +0545

    Revert "creating dropdown"
    This reverts commit 2405c936300051320dc0e36e4e0c458a9b80d406.

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -6,14 +6,9 @@ import { BsThreeDots } from "react-icons/bs";
 import { FaSearch } from "react-icons/fa";
 import Login from "../form/login";
 import QR from "../qr/qr";
-import { IoIosLogIn } from "react-icons/io";
-import { MdOutlineAdsClick } from "react-icons/md";
-import { BsBarChartLineFill } from "react-icons/bs";
-import { TbShoppingBag } from "react-icons/tb";
 const Navbar = () => {
   const [isLoginOpen, setLoginOpen] = useState(false);
-  const [isQROpen, setQROpen] = useState(false);
-  const [showDropdown, setShowDropdown] = useState(false);
+  const [isQROpen,setQROpen]=useState(false);
   return (
     <>
       <nav>
@@ -28,27 +23,13 @@ const Navbar = () => {
         </div>
 
         <div className="button-container">
-          <button className="get-app" onClick={() => { setQROpen(true) }}>
+          <button className="get-app" onClick={()=>{setQROpen(true)}}>
             <MdOutlineQrCodeScanner className="icon" /> Get App
           </button>
           <button className="log-in" onClick={() => { setLoginOpen(true) }}>Log In</button>
-          <div className="three-dot-container">
-            <button className="three-dot" onClick={() => setShowDropdown(!showDropdown)}>
-              <BsThreeDots />
-            </button>
-            {showDropdown && (
-              <div className="dropdown-menu">
-                <button onClick={() => setLoginOpen(true)}>
-                  <IoIosLogIn /> Login/Sign Up
-                </button>
-
-                <button onClick={() => alert("Advertise on Reddit Clicked")}><MdOutlineAdsClick />  Advertise on Reddit</button>
-                <button onClick={() => alert("Try Reddit Pro BETA Clicked")}><BsBarChartLineFill /> Try Reddit Pro BETA</button>
-                <button onClick={() => alert("Shop Collective Avatars Clicked")}><TbShoppingBag />  Shop Collective Avatars</button>
-
-              </div>
-            )}
-          </div>
+          <button className="three-dot">
+            <BsThreeDots />
+          </button>
         </div>
       </nav>
       <Login isOpen={isLoginOpen} onClose={() => setLoginOpen(false)} />
