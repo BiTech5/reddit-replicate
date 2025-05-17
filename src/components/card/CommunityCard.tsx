00cf46a8f0820232f23f05d73a2b06063c01519b
Bikram <bikikc7@gmail.com>
2025-05-17 16:49:31 +0545

    adding see more function

--- src/components/card/CommunityCard.tsx
+++ src/components/card/CommunityCard.tsx
@@ -22,8 +22,31 @@ const communities = [
         members: "713,649",
     }
 ];
-
+const more = [
+    {
+        name: "r/AskMen",
+        members: "6,746,736",
+    },
+    {
+        name: "r/AskWomen",
+        members: "5,555,673",
+    },
+    {
+        name: "r/PS4",
+        members: "5,533,527",
+    },
+    {
+        name: "r/apple",
+        members: "6,222,885",
+    },
+    {
+        name: "r/NBA2k",
+        members: "713,649",
+    }
+];
+import { useState } from "react";
 const CommunityCard = () => {
+    const [moreon, setMoreon] = useState(false);
     return (
         <div className="card-container">
             <h1>POPULAR COMMUNITIES</h1>
@@ -36,7 +59,20 @@ const CommunityCard = () => {
                     </div>
                 </div>
             ))}
-            <button className="see-more">See more</button>
+            {moreon && (more.map((commmunity, index) => (
+                <div className="group" key={index}>
+                    <img src="https://thispersondoesnotexist.com/" alt={`${commmunity.name} logo`} />
+                    <div className="info">
+                        <span className="name">{commmunity.name}</span>
+                        <span className="members">{commmunity.members} members</span>
+                    </div>
+                </div>
+
+            ))
+            )}
+            <button className="see-more" onClick={() => setMoreon(!moreon)}>See {moreon ? "less" : "more"}</button>
+
+
         </div>
     );
 };
